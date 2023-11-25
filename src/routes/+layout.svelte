e0c327b397cc4459ebf4d0fd7e3a26a33ea2e803
Wataru Sato <awwa@users.noreply.github.com>
2023-11-26 00:25:10 +0900

    add layout and checkbox

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,27 @@
 <script>
-  import Header from './Header.svelte';
-  import './styles.css';
+  import '../app.css';
 </script>
 
-<div class="app">
-  <Header />
-
-  <main>
-    <slot />
-  </main>
-
-  <footer>
-    <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-  </footer>
+<!-- header -->
+<div
+  class="fixed left-0 right-0 top-0 flex h-16 place-items-center items-center bg-slate-400 p-4 font-sans drop-shadow-md"
+>
+  <h1 class="flex-auto text-2xl">みんクル</h1>
+  <nav class="">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+  </nav>
 </div>
 
-<style>
-  .app {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
+<!-- body -->
+<article class="mt-16">
+  <slot />
+</article>
 
-  main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    width: 100%;
-    max-width: 64rem;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 12px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 12px 0;
-    }
-  }
-</style>
+<!-- footer -->
+<footer class="bg-slate-400">
+  <p>
+    <a href="">Privacy Policy</a>
+    <a href="">Terms of Service</a>
+  </p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,31 @@
-<script>
+<div class="flex flex-row">
+  <!-- sidenav -->
+  <div class="w-60">
+    <h2>
+      <span>メーカー名</span>
+    </h2>
+    <label class="table h-10 w-full hover:bg-slate-200 active:bg-slate-300">
+      <input class="table-cell h-10 w-4 align-middle accent-red-600" type="checkbox" id="mazda" />
+      <div class="table-cell w-full align-middle" for="mazda">マツダ</div>
+    </label>
+    <label class="table h-10 w-full hover:bg-slate-200 active:bg-slate-300">
+      <input class="table-cell h-10 w-4 align-middle" type="checkbox" id="toyota" />
+      <div class="table-cell w-full align-middle" for="toyota">トヨタ</div>
+    </label>
+    <label class="table h-10 w-full hover:bg-slate-200 active:bg-slate-300">
+      <input class="table-cell h-10 w-4 align-middle" type="checkbox" id="nissan" />
+      <div class="table-cell w-full align-middle" for="nissan">日産</div>
+    </label>
+  </div>
+  <!-- main -->
+  <div class="flex-auto bg-yellow-500">gggg</div>
+</div>
+
+<h1 class="text-3xl font-bold underline">Hello World</h1>
+<button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+<button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+
+<!-- <script>
   import App from './App.svelte';
   import Counter from './Counter.svelte';
   import welcome from '$lib/images/svelte-welcome.webp';
@@ -57,5 +84,11 @@
     height: 100%;
     top: 0;
     display: block;
+  }
+</style> -->
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
   }
 </style>
