689a24cc3d280933a395b2b16c3d93b2966ad7bc
Wataru Sato <awwa@users.noreply.github.com>
2024-01-14 08:42:00 +0900

    event test

--- apps/web/src/lib/component/Favorite.test.ts
+++ apps/web/src/lib/component/Favorite.test.ts
@@ -16,17 +16,53 @@ describe('Favorite.svelte', async () => {
     expect(button.innerHTML).not.toContain('text-yellow-500');
   });
 
-  test('click favorite', async () => {
-    const toggleFavorite = () => {};
-    const { getByLabelText } = render(Favorite, {
+  test('click favorite from false', async () => {
+    // Arrange
+    const toggleMock = vi.fn();
+    const { getByLabelText, component } = render(Favorite, {
       favorite: false,
-      toggle: toggleFavorite
+      toggle: toggleMock
     });
-    const button = getByLabelText('お気に入り');
-    expect(button.innerHTML).not.toContain('text-yellow-500');
-    await fireEvent.click(button);
-    expect(button.innerHTML).toContain('text-yellow-500');
-    await fireEvent.click(button);
-    expect(button.innerHTML).not.toContain('text-yellow-500');
+    const button1 = getByLabelText('お気に入り');
+    expect(button1.innerHTML).not.toContain('text-yellow-500');
+    // Act1
+    await fireEvent.click(button1);
+    // Assert1
+    expect(toggleMock).toHaveBeenCalledTimes(1);
+    expect(toggleMock).toHaveBeenCalledWith(true);
+    const button2 = getByLabelText('お気に入り');
+    expect(button2.innerHTML).toContain('text-yellow-500');
+    // Act2
+    await fireEvent.click(button2);
+    // Assert2
+    expect(toggleMock).toHaveBeenCalledTimes(2);
+    expect(toggleMock).toHaveBeenCalledWith(false);
+    const button3 = getByLabelText('お気に入り');
+    expect(button3.innerHTML).not.toContain('text-yellow-500');
+  });
+
+  test('click favorite from true', async () => {
+    // Arrange
+    const toggleMock = vi.fn();
+    const { getByLabelText } = render(Favorite, {
+      favorite: true,
+      toggle: toggleMock
+    });
+    const button1 = getByLabelText('お気に入り');
+    expect(button1.innerHTML).toContain('text-yellow-500');
+    // Act1
+    await fireEvent.click(button1);
+    // Assert1
+    expect(toggleMock).toHaveBeenCalledTimes(1);
+    expect(toggleMock).toHaveBeenCalledWith(false);
+    const button2 = getByLabelText('お気に入り');
+    expect(button2.innerHTML).not.toContain('text-yellow-500');
+    // Act2
+    await fireEvent.click(button2);
+    // Assert2
+    expect(toggleMock).toHaveBeenCalledTimes(2);
+    expect(toggleMock).toHaveBeenCalledWith(true);
+    const button3 = getByLabelText('お気に入り');
+    expect(button3.innerHTML).toContain('text-yellow-500');
   });
 });
