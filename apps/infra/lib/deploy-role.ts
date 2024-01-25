9a0003cc4b0f407818a4b2f4aa812c96b0f71187
Wataru Sato <awwa@users.noreply.github.com>
2024-01-25 22:48:38 +0900

    ECR_REGISTRY from secrets

--- apps/infra/lib/deploy-role.ts
+++ apps/infra/lib/deploy-role.ts
@@ -12,12 +12,6 @@ export class DeployRole extends Construct {
     const accountId = cdk.Stack.of(this).account;
     const oidcProvider = `arn:aws:iam::${accountId}:oidc-provider/token.actions.githubusercontent.com`;
 
-    const policy = new cdk.aws_iam.PolicyStatement({
-      effect: cdk.aws_iam.Effect.ALLOW,
-      actions: ["s3:PutObject", "s3:ListBucket"],
-      resources: ["*"],
-    });
-
     const role = new cdk.aws_iam.Role(this, "DeployRole", {
       assumedBy: new cdk.aws_iam.WebIdentityPrincipal(oidcProvider, {
         StringLike: {
@@ -27,6 +21,32 @@ export class DeployRole extends Construct {
       }),
       roleName: "DeployGitHub",
     });
+
+    const policy = new cdk.aws_iam.PolicyStatement({
+      effect: cdk.aws_iam.Effect.ALLOW,
+      actions: ["s3:PutObject", "s3:ListBucket"],
+      resources: ["*"],
+    });
     role.addToPolicy(policy);
+
+    const policy2 = new cdk.aws_iam.PolicyStatement({
+      effect: cdk.aws_iam.Effect.ALLOW,
+      actions: ["ecr:GetAuthorizationToken"],
+      resources: ["*"],
+    });
+    role.addToPolicy(policy2);
+
+    const policy3 = new cdk.aws_iam.PolicyStatement({
+      effect: cdk.aws_iam.Effect.ALLOW,
+      actions: [
+        "ecr:UploadLayerPart",
+        "ecr:PutImage",
+        "ecr:InitiateLayerUpload",
+        "ecr:CompleteLayerUpload",
+        "ecr:BatchCheckLayerAvailability",
+      ],
+      resources: ["*"],
+    });
+    role.addToPolicy(policy3);
   }
 }
