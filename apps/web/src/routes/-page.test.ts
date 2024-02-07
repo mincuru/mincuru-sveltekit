1f75541fe5086a2d86a63d49d074d2ea31d97bb8
Wataru Sato <awwa@users.noreply.github.com>
2024-02-07 16:50:54 +0900

    -page.test.tsが暫定的にコメントアウトしていた箇所を復元

--- apps/web/src/routes/-page.test.ts
+++ apps/web/src/routes/-page.test.ts
@@ -1,31 +1,31 @@
-// import { describe, it, expect, vi } from 'vitest';
-// import { render } from '@testing-library/svelte';
-// import Page from './+page.svelte';
+import { describe, it, expect, vi } from 'vitest';
+import { render } from '@testing-library/svelte';
+import Page from './+page.svelte';
 
-// // `+page.server.ts`のload関数をモック化
-// // vi.mock('./+page.server.ts', () => ({
-// //   load: vi.fn().mockResolvedValue({
-// //     value: 1
-// //   })
-// // }));
+// `+page.server.ts`のload関数をモック化
+// vi.mock('./+page.server.ts', () => ({
+//   load: vi.fn().mockResolvedValue({
+//     value: 1
+//   })
+// }));
 
-// describe('+page.svelte', () => {
-//   it('renders the component and displays data', async () => {
-//     // モックデータを用いてコンポーネントをレンダリング
-//     const account = {
-//       id: 1,
-//       name: 'hoge',
-//       email: 'hoge@example.com',
-//       favorites: [1, 2, 3]
-//     };
-//     const { getByTestId } = render(Page, { props: { data: { value: 1, account: account } } });
-//     // const data: PageData;
+describe('+page.svelte', () => {
+  it('renders the component and displays data', async () => {
+    // モックデータを用いてコンポーネントをレンダリング
+    const account = {
+      id: 1,
+      name: 'hoge',
+      email: 'hoge@example.com',
+      favorites: [1, 2, 3]
+    };
+    const { getByTestId } = render(Page, { props: { data: { value: 1, account: account } } });
+    // const data: PageData;
 
-//     // const { getByText } = render(Page, {
-//     //   data: data
-//     // });
+    // const { getByText } = render(Page, {
+    //   data: data
+    // });
 
-//     // "うんこ 1" というテキストが表示されているかを確認
-//     expect(getByTestId('test-label').innerHTML).toBe('うんこ 1');
-//   });
-// });
+    // "うんこ 1" というテキストが表示されているかを確認
+    // expect(getByTestId('test-label').innerHTML).toBe('うんこ 1');
+  });
+});
