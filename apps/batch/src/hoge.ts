340f05e2b8ea20773d84bbdf53ecf068036f47ba
Wataru Sato <awwa@users.noreply.github.com>
2024-03-17 17:29:18 +0900

    Update batch

---
+++ apps/batch/src/hoge.ts
@@ -0,0 +1,9 @@
+import { S3Event } from "aws-lambda";
+
+export const fuga = (price: number) => {
+  return price * 1.08;
+};
+
+export const fugaAsync = async (event: S3Event) => {
+  return await Promise.resolve(event);
+};
