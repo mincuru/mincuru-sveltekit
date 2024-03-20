5221b95074cce8945f7dbd23b9b85762ce074a08
Wataru Sato <awwa@users.noreply.github.com>
2024-03-20 14:34:45 +0900

    revert migrate-lambda

--- apps/migrate-lambda/__test__/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { handler } from "../index";
-import { Context } from "aws-lambda";
-
-const mockContext: Partial<Context> = {};
-
-test("Test handler", async () => {
-  const actual = await handler("hoge", mockContext as Context, jest.fn());
-});
