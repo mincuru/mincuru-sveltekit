3cf7558d06d4d7cc0948bb511baf7d53dd39c162
Wataru Sato <awwa@users.noreply.github.com>
2024-03-21 00:28:00 +0900

    Add docs by gatsby

--- apps/infra/bin/infra.ts
+++ apps/infra/bin/infra.ts
@@ -2,7 +2,9 @@
 import * as cdk from "aws-cdk-lib";
 import { InfraStack } from "../lib/infra-stack/infra-stack";
 import { TestStack } from "../lib/test-stack/test-stack";
+import { DocsStack } from "../lib/docs-stack/docs-stack";
 
 const app = new cdk.App();
 new InfraStack(app, "InfraStack");
 new TestStack(app, "TestStack");
+new DocsStack(app, "DocsStack");
